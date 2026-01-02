6d1a0e4264f7114f62fc43740278ec056c6297cc
Mikael312 <damino898989@gmail.com>
2026-01-02 13:06:42 +0800

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -2977,4 +2977,3 @@ ArcadeUILib:AddToggleRow("Xray Base", toggleXrayBase, "Fps Boost", toggleFpsBoos
 ArcadeUILib:AddToggleRow("Esp Timer", toggleEspTimer, "", nil)
 
 print("🎮 Arcade UI with ESP, Base Line, Anti Turret, Aimbot, Kick Steal, Unwalk Anim, Auto Steal, Anti Debuff, Anti Rdoll, Xray Base, Fps Boost & Esp Timer Loaded Successfully!")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mikael312/StealBrainrot/refs/heads/main/Sabstealtoolsv1.lua"))()
