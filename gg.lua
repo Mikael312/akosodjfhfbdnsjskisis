70e22cec876fac5163518c7a0c8c1b301a1843a1
Mikael312 <damino898989@gmail.com>
2026-04-22 15:33:39 +0800

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -35,7 +35,7 @@ local S = {
     Lighting = game:GetService("Lighting"),
 }
 
-local LocalPlayer = Players.LocalPlayer
+local player = S.Players.LocalPlayer
 
 local Packages = S.ReplicatedStorage:WaitForChild("Packages")
 local Datas = S.ReplicatedStorage:WaitForChild("Datas")
